be5ddb18dc7247591638b2ba131d62951ed8fb0a
JP Barbosa <jp@jp7.com.br>
2022-08-21 00:06:12 -0300

    Add Rekognition logo in the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,15 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+import { ReactComponent as Rekognition } from "../../public/rekognition.svg";
+
 export const Header: React.FC = () => {
   return (
     <header>
-      <h1>SST Rekognition</h1>
-      <p>An event-driven app to label images on top of AWS</p>
+      <Rekognition />
+      <div>
+        <h1>SST Rekognition</h1>
+        <p>An event-driven app to label images on top of AWS</p>
+      </div>
     </header>
   );
 };
